5b9c974317ee8b1591ef158027ce62bba3a31d10
ShangYJQ <421207553@qq.com>
2026-04-26 16:56:29 +0800

    feat(blink-pairs, render-markdown): add rainbow & completion

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,1 +1,11 @@
-require("render-markdown").setup({ latex = { enabled = false } })
+require("render-markdown").setup({
+	latex = { enabled = false },
+	completions = {
+		-- Settings for blink.cmp completions source
+		blink = { enabled = true },
+		-- Settings for coq_nvim completions source
+		coq = { enabled = false },
+		-- Settings for in-process language server completions
+		lsp = { enabled = true },
+	},
+})
