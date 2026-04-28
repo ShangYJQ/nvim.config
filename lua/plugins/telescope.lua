8b2575f03825a9454922ded86573952cb824f7ed
ShangYJQ <421207553@qq.com>
2026-04-28 16:23:57 +0800

    refactor(keymap): comment splits, add telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,4 +1,7 @@
 -- Telescope set
+
+local actions = require("telescope.actions")
+
 require("telescope").setup({
 	defaults = {
 		layout_strategy = "horizontal",
@@ -14,6 +17,19 @@ require("telescope").setup({
 			height = 0.9,
 
 			preview_width = 0.5,
+		},
+
+		-- Default configuration for telescope goes here:
+		-- config_key = value,
+		mappings = {
+			i = {
+				-- map actions.which_key to <C-h> (default: <C-/>)
+				-- actions.which_key shows the mappings for your picker,
+				-- e.g. git_{create, delete, ...}_branch for the git_branches picker
+				["<C-j>"] = actions.move_selection_next,
+				["<C-k>"] = actions.move_selection_previous,
+				["<C-l>"] = actions.select_default,
+			},
 		},
 	},
 	extensions = {
